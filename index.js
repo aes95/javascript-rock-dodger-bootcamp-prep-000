cc2ec2cbf1b711b139ed3c16aa0e82fbc25d2a3e
Aviad Sapir <aviadsapir@gmail.com>
2018-10-08 01:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -132,7 +132,17 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-
+    if (e.which === LEFT_ARROW) {
+      e.stopPropagation();
+      e.preventDefault();
+      moveDodgerLeft();
+    }
+    else if(e.which === RIGHT_ARROW){
+      e.stopPropagation();
+      e.preventDefault();
+      moveDodgerRight();
+    }
+  }
 
 function moveDodgerLeft() {
   // implement me!
